2c53664067bfb8daca5243db721ea7a3a7ecff61
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-01-15 01:34:49 +0530

    updating custom.js

---
+++ coustom/custom.js
@@ -0,0 +1,31 @@
+customElements (reactElement,mainContainer)
+{
+
+    // const domElement=document.createElement(reactElement.type)
+    // domElement.innerHTML=reactElement.children
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
+    // mainContainer.appendchild(domElement)
+    
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTML=reactElement.children
+    for (const prop in props)
+    {
+        if (prop==children) continue;
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
+
+}
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'click me'
+}
+
+
+
+const mainContainer=document.querySelector('#root')
+customRender(reactElement,mainContainer)
