e020affcfe027cc49c0f25b8e8763324f03e751a
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-01-16 11:23:42 +0530

    making project03

--- project02/src/App.jsx
+++ project02/src/App.jsx
@@ -2,15 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    username:"Debarima",
+    age:21
+  }
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl' >tailwind test</h1>
-    </>
+    <h1 className='bg-green-600 text-black rounded-xl'  
+    > tailwind test</h1>
+    <Card username="chai aur code" someobj={myObj}/>
+    <Card/>
+      
+     </>
   )
 }
+
+
+
+   
+  
+
 
 export default App
