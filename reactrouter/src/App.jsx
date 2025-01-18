db0627d66075ff34319b26484bfe2e962c489a01
DebarimaBasu <151932080+DebarimaBasu@users.noreply.github.com>
2025-01-18 22:29:49 +0530

    react router comp

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/header'
+import Home from './components/home/Home'
+import Footer from './components/footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-700 p-6 text-yellow-50'> react router</h1>
+     <Header/>
+     <Home/>
+     <Footer/>
     </>
   )
 }
--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -1,10 +1,49 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import About from './components/about/about'
+import Home from './components/home/Home'
+import Contact from './components/contact/Contact'
+import User from './components/user/User'
+import Github from './components/github/Github'
+//import App from './App.jsx'
+// const router=createBrowserRouter([{// creating an object
+//   path:'/',
+//   element:<Layout/>,
+//   children:[
+//     {
+//       path:'/',
+//       element:<Home/>
+//     },
+//     {
+//       path:'about',
+//       element:<About/>
+//     },
+//     {
+//       path:'contact',
+//       element:<Contact/>
+
+//     }
+//   ]
+// }])
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route path='github' element={<Github/>}/>
+
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
